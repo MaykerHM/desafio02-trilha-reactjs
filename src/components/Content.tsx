14041dd4a43227df97e71209a8f6fc01dc6316a5
MaykerHM <hydeki8@gmail.com>
2021-03-15 17:24:05 -0300

    Criação de componentes, Header, Content e sideBar.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard'
+import { Header } from '../components/Header'
+
+interface ContentProps {
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({ selectedGenre, movies }: ContentProps) {
+  return (
+    <div className="container">
+      <Header selectedGenre={ selectedGenre } />
+
+      <main>
+        <div className="movies-list">
+          { movies.map(movie => (
+            <MovieCard title={ movie.Title } poster={ movie.Poster } runtime={ movie.Runtime } rating={ movie.Ratings[0].Value } />
+          )) }
+        </div>
+      </main>
+    </div>
+  )
 }
